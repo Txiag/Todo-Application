4553cec5d6ba57d286c956cadba80327598fe64a
Thiago do Nascimento <thiago.nasf@gmail.com>
2021-08-26 00:34:08 -0300

    Adiciona campo de busca

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -1,37 +1,32 @@
 const { todo } = require("../models");
+const { Op } = require("sequelize");
 const errorMessage = require("../db/errors");
+
 const Create = async (req, res) => {
   delete req.body.id; // Deleta atributo id caso seja enviado na requisição
   try {
     const created = await todo.create(req.body);
     res.status(201).json(created);
   } catch (err) {
+    console.log(err);
     res.status(400).json(errorMessage(err));
   }
 };
 
 const List = async (req, res) => {
-  const { page = 1, itemsPerPage = 20 } = req.query; // Pega página e quantidade de itens dos query parameters ou usa default caso não exista
+  const { page = 1, itemsPerPage = 20, q = "" } = req.query; // Pega página e quantidade de itens dos query parameters ou usa default caso não exista
   const { count: itemCount, rows: items } = await todo.findAndCountAll({
     limit: itemsPerPage,
     offset: (page - 1) * itemsPerPage,
+    where: {
+      searchTerm: {
+        [Op.substring]: `%${q}%`,
+      },
+    },
   });
-  const nextPage =
-    page * itemsPerPage >= itemCount
-      ? null
-      : `http://localhost:8000/todos?page=${
-          Number(page) + 1
-        }&offset=${itemsPerPage}`;
-  const previousPage =
-    page - 1 <= 0
-      ? null
-      : `http://localhost:8000/todos?page=${
-          Number(page) - 1
-        }&offset=${itemsPerPage}`;
+
   res.json({
     page,
-    nextPage,
-    previousPage,
     itemCount,
     itemsPerPage,
     items,
@@ -50,9 +45,11 @@ const Edit = async (req, res) => {
   const found = await todo.findByPk(id);
   if (!found) return res.status(404).json({ error: "Não encontrado" }); // Retorna erro caso registro não exista
   delete req.body.id; // Apaga atributo de id no corpo para prevenir sobrescrita
+  delete req.body.searchTerm; // Apaga atributo do termo de busca
   found.dataValues = {
     ...found.dataValues,
     ...req.body,
+    searchTerm: CreateSearchTerm(req.body.title, req.body.description),
     updatedAt: new Date().toISOString(), // Sobrescreve o updatedAt antigo
   };
   try {
@@ -62,7 +59,6 @@ const Edit = async (req, res) => {
     res.status(400).json(errorMessage(err));
   }
 };
-
 module.exports = {
   Create,
   List,
--- backend/models/todo.js
+++ backend/models/todo.js
@@ -1,43 +1,66 @@
 const Sequelize = require("sequelize");
 const { isStringOrNull, isBoolean, isNotNull } = require("../db/validators");
 const database = require("../db/sequelize");
+const deburr = require("lodash.deburr");
 
-const Todo = database.define("todo", {
-  id: {
-    type: Sequelize.UUID,
-    defaultValue: Sequelize.UUIDV4,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: true,
-    defaultValue: null,
-    validate: {
-      isStringOrNull,
+const Todo = database.define(
+  "todo",
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
     },
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: true,
-    defaultValue: null,
-    validate: {
-      isStringOrNull,
-      bothNullOrEmpty: (value) => {
-        if (!value && !this.title)
-          throw new Error(
-            "Campo de descrição e título não podem estar vazios ao mesmo tempo"
-          );
+    title: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: null,
+      validate: {
+        isStringOrNull,
       },
     },
-  },
-  done: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-    validate: {
-      isBoolean,
+    description: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: null,
+      validate: {
+        isStringOrNull,
+        bothNullOrEmpty: (value) => {
+          if (!value && !this.title)
+            throw new Error(
+              "Campo de descrição e título não podem estar vazios ao mesmo tempo"
+            );
+        },
+      },
+    },
+    done: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      validate: {
+        isBoolean,
+      },
+    },
+    searchTerm: {
+      type: Sequelize.STRING,
+      defaultValue: "",
+      allowNull: false,
     },
   },
-});
+  {
+    hooks: {
+      beforeSave(todo) {
+        // Hook para adicionar o termo de busca ao salvar um registro
+        const title = todo.title || "";
+        const description = todo.description || "";
+        todo.searchTerm = deburr(title + description);
+      },
+    },
+    defaultScope: {
+      // Excluir termo de busca das queries
+      attributes: { exclude: ["searchTerm"] },
+    },
+  }
+);
 
 module.exports = Todo;
